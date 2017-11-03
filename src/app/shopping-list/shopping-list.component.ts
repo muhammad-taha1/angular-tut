f874272de1e94f2a285bb5d81fc5164066d336db
muhammad-taha1 <muhammad.taha@mail.mcgill.ca>
2017-11-03 00:00:49 -0400

    updated to project

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingridient } from '../shared/ingridient.model'
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
-  ingridients = [];
+  ingridients: Ingridient[] = [
+    new Ingridient('Apple', 5),
+    new Ingridient('Tomato', 10)
+  ];
   
   constructor() { }
 
