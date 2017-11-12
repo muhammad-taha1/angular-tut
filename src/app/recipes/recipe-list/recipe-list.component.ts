80e977b7dbf440555965f3c6fbda9e75e40a5048
muhammad-taha1 <muhammad.taha@mail.mcgill.ca>
2017-11-12 18:40:02 -0500

    updated recipe section

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,23 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('test recipe', 'this is simply a test',
+    'https://c.pxhere.com/photos/a8/83/sweet_struffoli_typical_sweet_meal_food_recipe_kitchen_dish-754482.jpg!d'),
+    new Recipe('test recipe 2', 'this is simply a test',
     'https://c.pxhere.com/photos/a8/83/sweet_struffoli_typical_sweet_meal_food_recipe_kitchen_dish-754482.jpg!d')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe : Recipe) {
+    this.recipeWasSelected.emit(recipe);
+
   }
 
 }
